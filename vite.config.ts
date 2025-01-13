3850000ae07cad2f14fd91a7efc204ed5a13b676
Taisiiya Manshina <taisiiyam@gmail.com>
2025-01-13 13:11:23 +1000

    first

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import * as path from "node:path";
+
+// https://vite.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, 'src'),
+      '&': path.resolve(__dirname, './'),
+    },
+  },
+  plugins: [react()],
+})
