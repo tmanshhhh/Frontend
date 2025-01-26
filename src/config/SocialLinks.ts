65171fe199248a30e93e28f07e9dd6c587197bfe
tmanshhhh <taisiiyam@gmail.com>
2025-01-26 21:41:52 +1000

    Ссылки на социальные сети вынесены в отдельный файл, общие пропсы вынесены в константу

---
+++ src/config/SocialLinks.ts
@@ -0,0 +1,19 @@
+import {faVk, faTelegram, faInstagram} from "@fortawesome/free-brands-svg-icons";
+
+export const SocialLinks = [
+    {
+        href: "https://instagram.com/tmanshhhh",
+        label: "Instagram",
+        icon: faInstagram,
+    },
+    {
+        href: "https://vk.com/tmanshhhh",
+        label: "VK",
+        icon: faVk,
+    },
+    {
+        href: "https://t.me/tmanshhhh",
+        label: "Telegram",
+        icon: faTelegram,
+    },
+];
