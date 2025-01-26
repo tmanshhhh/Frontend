03f2b6f9e1a8c817711237d550f7870535290bf5
tmanshhhh <taisiiyam@gmail.com>
2025-01-26 21:41:52 +1000

    Ссылки на социальные сети вынесены в отдельный файл, общие пропсы вынесены в константу

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,22 +1,23 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faVk, faTelegram, faInstagram} from "@fortawesome/free-brands-svg-icons";
 import {FooterContainer} from "@ styles/FooterStyle/FooterContainer.tsx";
 import {IconLinks} from "@ styles/FooterStyle/IconLinks.tsx";
 import {IconLink} from "@ styles/FooterStyle/IconLink.tsx";
+import {SocialLinks} from "@/config/SocialLinks.ts"
+
+const baseLinkProps = {
+    target: "_blank",
+    rel: "noopener noreferrer",
+};
 
 export const Footer: React.FC = () => {
     return (
         <FooterContainer>
             <IconLinks>
-                <IconLink href="https://instagram.com/tmanshhhh" target="_blank" rel="noopener noreferrer" aria-label="VK">
-                    <FontAwesomeIcon icon={faInstagram} />
-                </IconLink>
-                <IconLink href="https://vk.com/tmanshhhh" target="_blank" rel="noopener noreferrer" aria-label="VK">
-                    <FontAwesomeIcon icon={faVk} />
-                </IconLink>
-                <IconLink href="https://t.me/tmanshhhh" target="_blank" rel="noopener noreferrer" aria-label="Telegram">
-                    <FontAwesomeIcon icon={faTelegram} />
-                </IconLink>
+                {SocialLinks.map(({ href, label, icon }) => (
+                    <IconLink key={href} href={href} {...baseLinkProps} aria-label={label}>
+                        <FontAwesomeIcon icon={icon} />
+                    </IconLink>
+                ))}
             </IconLinks>
         </FooterContainer>
     );
